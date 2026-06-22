203e98bb381b5b6bfd5510beae582eda490d6c47
roccoministeri6-wq <roccoministeri6@gmail.com>
2026-06-22 02:24:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -35,3 +35,73 @@ fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en")
 });
 
 }
+const languages = [
+    "Italiano",
+    "Inglese",
+    "Spagnolo",
+    "Russo"
+];
+
+const qualities1 = [
+    "Dedizione",
+    "Leadership",
+    "Empatia",
+    "Problem Solving"
+];
+
+const qualities2 = [
+    "Motivazione",
+    "Teamwork",
+    "Public Speaking",
+    "Comunicazione"
+];
+
+let languageIndex = 0;
+let qualityIndex1 = 0;
+let qualityIndex2 = 0;
+
+const languageElement = document.getElementById("languages-rotate");
+const qualityElement1 = document.getElementById("skill-rotate-1");
+const qualityElement2 = document.getElementById("skill-rotate-2");
+
+if(languageElement){
+
+    setInterval(() => {
+
+        languageIndex =
+        (languageIndex + 1) % languages.length;
+
+        languageElement.innerText =
+        languages[languageIndex];
+
+    }, 2000);
+
+}
+
+if(qualityElement1){
+
+    setInterval(() => {
+
+        qualityIndex1 =
+        (qualityIndex1 + 1) % qualities1.length;
+
+        qualityElement1.innerText =
+        qualities1[qualityIndex1];
+
+    }, 2000);
+
+}
+
+if(qualityElement2){
+
+    setInterval(() => {
+
+        qualityIndex2 =
+        (qualityIndex2 + 1) % qualities2.length;
+
+        qualityElement2.innerText =
+        qualities2[qualityIndex2];
+
+    }, 2000);
+
+}
